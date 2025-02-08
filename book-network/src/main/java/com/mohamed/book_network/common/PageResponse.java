76c4a6f3f9aac914ead376161a7349da41b1a4c2
Mohamed Hanafy <mohamed.hanafy.mostafa@gmail.com>
2025-02-08 21:01:36 +0200

    someBookFeatures

---
+++ book-network/src/main/java/com/mohamed/book_network/common/PageResponse.java
@@ -0,0 +1,4 @@
+package com.mohamed.book_network.common;
+
+public class PageResponse {
+}
---
+++ book-network/src/main/java/com/mohamed/book_network/history/BookTransactionHistoryRepository.java
@@ -0,0 +1,4 @@
+package com.mohamed.book_network.history;
+
+public interface BookTransactionHistoryRepository {
+}
